3d2ecd01a7fade262fbf0ce1efd8e22a97c2368f
Vipin Mallaya <vkgopinathan@live.com>
2021-11-05 00:21:01 +0530

    Index Accessing

--- Program.cs
+++ Program.cs
@@ -10,9 +10,10 @@ namespace CodeNuggets
         {
             Console.WriteLine("Hello World!");
 
-            GoodVariableDeclaration();
-            BadVariableDeclaration();
+            IndexAccessing();
+            TraditionalAccessing();
         }
+        #region StringCreation
 
         static void BadStringOperation()
         {
@@ -40,7 +41,9 @@ namespace CodeNuggets
 
             Console.WriteLine("Joined String {0}", sampleString.ToString());
         }
+        #endregion
 
+        #region StringConcatination
         static void BadStringConcatinationOperation()
         {
             var inputString = "Test1";
@@ -56,7 +59,9 @@ namespace CodeNuggets
 
             Console.WriteLine("Joined String {0}", outPutString);
         }
+        #endregion
 
+        #region Linq-FirstOrDefault
         static List<KeyValuePair<string, string>> NameAndGrade = new List<KeyValuePair<string, string>>()
         {
             new KeyValuePair<string,string>("Name1","A"),
@@ -65,24 +70,26 @@ namespace CodeNuggets
         };
 
         static void GoodWorkingWithListForOneItem()
-        { 
+        {
             var nameWithGradeC = NameAndGrade.FirstOrDefault(item => item.Value == "C");
             Console.WriteLine("Name With Grade C {0}", nameWithGradeC.Key);
         }
 
         static void BadWorkingWithListForOneItem()
-        { 
+        {
             var result = NameAndGrade.Where(item => item.Value == "C");
             var nameWithGradeC = result.FirstOrDefault();
             Console.WriteLine("Name With Grade C {0}", nameWithGradeC.Key);
         }
+        #endregion
 
+        #region VariableDeclaration
         static void GoodVariableDeclaration()
         {
             var intVar = default(int); // Declare int with default value - 0 in it;
-            var stringVar = string.Empty; 
+            var stringVar = string.Empty;
 
-            Console.WriteLine("Defaults values for int - {0} and string {1}", intVar,stringVar);
+            Console.WriteLine("Defaults values for int - {0} and string {1}", intVar, stringVar);
         }
         static void BadVariableDeclaration()
         {
@@ -91,5 +98,79 @@ namespace CodeNuggets
 
             Console.WriteLine("Defaults values for int - {0} and string {1}", intVar, stringVar);
         }
+        #endregion
+
+        #region ConditionalOperator
+        static void GoodConditionalOperation(bool condition, int input)
+        {
+            if (condition)
+            {
+                Console.WriteLine("Condition is true");
+            }
+
+            var result = input == 2;
+            Console.WriteLine("Condition Evaluation {0}", result);
+        }
+
+        static void BadConditionalOperation(bool condition, int input)
+        {
+            if (condition == true)
+            {
+                Console.WriteLine("Condition is true");
+            }
+
+            var result = input == 2 ? true : false;
+
+            Console.WriteLine("Condition Evaludation {0}", result);
+        }
+        #endregion
+
+        #region ObjectCreation
+        static void NewObjectCreation()
+        {
+            List<string> newObject = null;
+            //...
+            //...
+            newObject ??= new List<string>();
+        }
+
+        static void TraditionalObjectCreation()
+        {
+            List<string> newObject = null;
+            //...
+            //...
+            if (newObject == null)
+            {
+                newObject = new List<string>();
+            }
+        }
+        #endregion
+
+
+        #region Index Accessing
+        static string[] Items = new string[] { "Item1", "Item2", "Item3", "Item4" };
+        static void IndexAccessing()
+        {
+            for (int i = 1; i <= Items.Length; i++)
+            {
+                Console.Write($"{Items[^i]} ");
+            }
+            //Output 
+            //Item4 Item3 Item2 Item1 
+        }
+
+        static void TraditionalAccessing()
+        {     
+            for (int i = Items.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{Items[i]} ");
+            } 
+            //Output 
+            //Item4 Item3 Item2 Item1 
+        }
+        #endregion
+
+
+
     }
 }
